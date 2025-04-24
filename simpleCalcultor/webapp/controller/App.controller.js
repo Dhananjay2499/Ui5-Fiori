fa496fa3f97036d449acb84bb54ccf6794fd00b4
Dhananjay Kulkarni <dhananjaykulkarni2499@gmail.com>
2025-04-24 14:53:37 +0000

    commit

--- simpleCalcultor/webapp/controller/App.controller.js
+++ simpleCalcultor/webapp/controller/App.controller.js
@@ -1,19 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast"
-], function(Controller,MessageToast) {
+], function (Controller, MessageToast) {
     "use strict";
     return Controller.extend("path.calculator.controller.App", {
+
         onPressOfButton(var1) {
-            if (var1 === "AC"){
+            var value = this.byId("valInput").getValue();
+            if (var1 === "AC") {
                 this.byId("valInput").setValue('');
                 new MessageToast.show('Input cleared!!');
-            }else if (var1 === "+"){
-                // MessageToast.show('+ Button clicked!');
-                // this.byId("valInput").setHeight("200px");
-                this.byId("valInput").addStyleClass("expanded");
-            }else{
-                this.byId("valInput").setValue(this.byId("valInput").getValue() + var1);
+            } 
+            else {
+            return this.byId("valInput").setValue( value + var1);
+                
             }
         },
 
@@ -21,9 +21,16 @@ sap.ui.define([
         onPressOfBack() {
 
             var input = this.byId("valInput").getValue();
-            this.byId("valInput").setValue(input.substr(0,input.length-1));
-            // this.substr(0,input.length-1).setValue();
+            this.byId("valInput").setValue(input.substr(0, input.length - 1));
 
-        }
+        },
+        onPressOfOperation(varSign) {
+            var value = this.byId("valInput").getValue();
+            if (/[+\-*/]/.test(value)) {
+                return this.byId("valInput").setValue(value.replace(/[+\-*/]/g, varSign));
+            } else {
+                return this.byId("valInput").setValue(value + "\n" + varSign);
+            }
+        },
     });
 });
