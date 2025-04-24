f3a0a1ad9478ea2f6d6b65d2986655c2385bd8be
Dhananjay Kulkarni <dhananjaykulkarni2499@gmail.com>
2025-04-24 07:17:47 +0000

    ui5.walkthrough

--- ui5.walkthrough/webapp/Component.js
+++ ui5.walkthrough/webapp/Component.js
@@ -1,15 +1,16 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+    // "sap/ui/model/resource/ResourceModel"
+], function (UIComponent, JSONModel) {
     "use strict";
     return UIComponent.extend("ui5.walkthrough.Component", {
         metadata: {
             "interfaces": ["sap.ui.core.IAsyncComponentCreation"],
-            "rootView": "ui5.walkthrough.view.App",
-            "type": "XML",
-            "id": "app"
+            // "rootView": "ui5.walkthrough.view.App",
+            // "type": "XML",
+            // "id": "app"
+            manifest : "json"
         },
         
         init(){
@@ -26,11 +27,12 @@ sap.ui.define([
             const oModel = new JSONModel(oData);
             this.setModel(oModel);
 
-            // set i18n model
-            const i18nModel = new ResourceModel({
-                bundleName : "ui5.walkthrough.i18n.i18n"
-            });
-            this.setModel(i18nModel,"i18n");
+            // Not required once you configure it in manifest.json
+            // set i18n model 
+            // const i18nModel = new ResourceModel({
+            //     bundleName : "ui5.walkthrough.i18n.i18n"
+            // });
+            // this.setModel(i18nModel,"i18n");
 
 
         }
