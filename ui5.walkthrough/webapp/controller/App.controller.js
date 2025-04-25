d457d38aec0c1cde7289428c06057af2882efcb6
Dhananjay Kulkarni <dhananjaykulkarni2499@gmail.com>
2025-04-25 04:44:15 +0000

    commit

--- ui5.walkthrough/webapp/controller/App.controller.js
+++ ui5.walkthrough/webapp/controller/App.controller.js
@@ -1,9 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    // "sap/m/MessageToast",
+    // "sap/ui/model/json/JSONModel",
+    // "sap/ui/model/resource/ResourceModel"
+], function (Controller){ //, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("ui5.walkthrough.controller.App", {
         // without Component Configuration
@@ -43,11 +43,11 @@ sap.ui.define([
 
         // with Component Configuration
 
-        onClick(){
-            const oBundle = this.getView().getModel("i18n").getResourceBundle();
-            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            const sMsg = oBundle.getText("helloMsg",[sRecipient]);
-        }
+        // onClick(){
+        //     const oBundle = this.getView().getModel("i18n").getResourceBundle();
+        //     const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+        //     const sMsg = oBundle.getText("helloMsg",[sRecipient]);
+        // } Moved to HelloPanel copntroller
 
     });
 });
---
+++ ui5.walkthrough/webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast) {
+    "use strict";
+
+    return Controller.extend("ui5.walkthrough.controller.HelloPanel", {
+
+
+        onClick() {
+            const oBundle = this.getView().getModel("i18n").getResourceBundle();
+            const sRecipient = this.getView().getModel().getProperty("/recipient/name");
+            const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+        }
+
+
+    })
+
+})
