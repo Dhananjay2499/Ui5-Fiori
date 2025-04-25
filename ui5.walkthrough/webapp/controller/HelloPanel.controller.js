c0537ffbdab4142256e058759072ac8c2fad35ca
Dhananjay Kulkarni <dhananjaykulkarni2499@gmail.com>
2025-04-25 08:44:38 +0000

    commit

--- ui5.walkthrough/webapp/controller/HelloPanel.controller.js
+++ ui5.walkthrough/webapp/controller/HelloPanel.controller.js
@@ -11,6 +11,27 @@ sap.ui.define([
             const oBundle = this.getView().getModel("i18n").getResourceBundle();
             const sRecipient = this.getView().getModel().getProperty("/recipient/name");
             const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+            MessageToast.show(sMsg);
+        },
+
+
+        // Dialog Box
+        async onOpenDialog(){
+            this.oDialog ??= await this.loadFragment({
+                name:"ui5.walkthrough.view.HelloDialog"
+            });
+
+            this.oDialog.open();
+
+        },
+
+        //Close Dialog Box
+        onCloseDialog(){
+            // note: We don't need to chain to the pDialog promise, since this event handler
+			// is only called from within the loaded dialog itself.
+
+            this.byId("helloDialog").close();
+
         }
 
 
